72e2afaa78dae7c1abaca8c038548deb696d995f
DrGabriel <gabriel_santosgso@yahoo.com.br>
2017-02-02 14:48:20 -0200

    Att 1.1
    busca e escrita de espaços vazios no arquivo pronta entretanto só testado com longs e ints, falta testar na pagina

--- arvoreBJava/arquivoFerramentas/EspacoVazio.java
+++ arvoreBJava/arquivoFerramentas/EspacoVazio.java
@@ -1,4 +1,4 @@
-Espackage arquivoFerramentas;
+package arquivoFerramentas;
 
 import java.io.*;
 import java.util.RandomAccess;
@@ -7,22 +7,68 @@ import java.util.RandomAccess;
  * Created by gabriel on 30/01/17.
  */
 public class EspacoVazio {
-    private static String filePath = "espacos.bin";
+    private String filePath = "espacos.bin";
+    private  RandomAccessFile file;
+    public EspacoVazio(){
+        try {
+            this.file = new RandomAccessFile(filePath,"rw");
+            this.file.writeInt(0);
+            this.file.writeLong(0);
+            this.file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    public static long buscaEspacoVazio(){
-        RandomAccessFile file;
+    }
+    public void escreveOffsetEspVazio(long offset){
+        try {
+            file = new RandomAccessFile(filePath,"rw");
+            int qtdOffsets = file.readInt();
+            file.seek(file.length());
+            file.writeLong(offset);
+            file.seek(0);
+            file.writeInt(qtdOffsets + 1);
+            this.file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public long buscaEspacoVazio(RandomAccessFile bTree){
         RandomAccessFile file2;
         long offSetEspacoVazio = 0;
         int qtdOffsets= -1;
         try {
             file = new RandomAccessFile(filePath,"r");
-            file2 = new RandomAccessFile(filePath + ".temp","w");
+            file2 = new RandomAccessFile(filePath + ".temp","rw");
             qtdOffsets = file.readInt();
             offSetEspacoVazio = file.readLong();
-            file.writeInt(qtdOffsets -1);
-            for(int i =0;i<qtdOffsets - 1;i++){
-                file2.writeLong(file.readLong());
+            if(qtdOffsets -1 == 0){//n tem mais espacos vazios, por algum motivo '-'
+                file2.writeInt(qtdOffsets + 1);
+                bTree.seek(bTree.length());
+                file2.writeLong(bTree.getFilePointer());
+                bTree.seek(bTree.length() + 4076);// odeio constantes, mas n tem oq fazer
+                file2.writeLong(bTree.getFilePointer());
+            }else{
+                file2.writeInt((qtdOffsets -1));
+                for(int i =0;i<qtdOffsets - 1;i++){
+                    file2.writeLong(file.readLong());
+                }
             }
+
+            file.close();
+            file2.close();
+            File file1 = new File(filePath);
+            File fileTemp = new File(filePath+ ".temp");
+            boolean successful = fileTemp.renameTo(file1);
+            if(successful == false){
+                System.out.println("Deu ruim lesk!");
+            }
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
